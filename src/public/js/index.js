5d5052c6ad71570b257f3b5e65768a52bc132c6b
Tuấn Anh <81815569+ongcaoboi@users.noreply.github.com>
2022-05-02 14:04:56 +0700

    Update index.js

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -2,7 +2,7 @@ var res = "";
 var i = 0;
 var time_start = 0;
 var width = 0;
-var pattern = /^([0-9]+\s)*$/;
+var pattern = /^([0-9]+\s)*[0-9]+|([0-9]+\s)$/;
 document.getElementById('run').addEventListener("click", async (e) => {
     e.preventDefault();
     document.getElementById('main').innerHTML = "";
@@ -18,6 +18,38 @@ document.getElementById('run').addEventListener("click", async (e) => {
         document.getElementById('run').style.display = 'inline-block';
         return;
     }
+    var lines = input_.trim().split(/\r\n|\n/);
+    let line1 = lines.shift().split(' ');
+    var inputIs = false;
+    if (line1[0] != lines.length) {
+        inputIs = true;
+    }
+    var last_item = 0;
+    var last_name = 0;
+    lines.forEach(element => {
+        element = element.split(' ');
+        if (last_item > Number(element[1])) {
+            inputIs = true;
+            return false;
+        }
+        if (last_name >= Number(element[0])) {
+            inputIs = true;
+            return false;
+        }
+        last_name = Number(element[0]);
+        last_item = Number(element[1]);
+        if (typeof element[0] === 'undefined' | typeof element[1] === 'undefined' | typeof element[2] === 'undefined') {
+            inputIs = true;
+            return false;
+        }
+    });
+
+    if (inputIs) {
+        alert("Hey, data input is not in the correct format!");
+        document.getElementById('run').style.display = 'inline-block';
+        return;
+    }
+
     $.ajax({
         url: '/getRobin',
         type: 'post',
@@ -53,22 +85,27 @@ function myLoop(data) {
     setTimeout(function () {
         var element = data[i];
         var item = document.createElement("div");
+        console.log(element);
+        if (element.process.id == "wait") {
+            item.classList.add('item-wait');
+        }
         if (i == 0) {
-            item.className = "item";
+            item.classList.add("item");
             let liner = element.process.id + '<div class="liner">' + element.time_start + '</div>';
             item.innerHTML = liner;
         } else {
             var last_element = data[i - 1];
             if (element.process.id == last_element.process.id) {
-                item.className = "item item-next";
-                if (data.length == i+1) {
-                    let liner = '<div class="liner-end">' + Number(element.time_start+1) + '</div>';
+                item.classList.add("item");
+                item.classList.add("item-next")
+                if (data.length == i + 1) {
+                    let liner = '<div class="liner-end">' + Number(element.time_start + 1) + '</div>';
                     item.innerHTML = liner;
                 }
             } else {
-                item.className = "item";
-                if (data.length == i+1) {
-                    let liner = element.process.id + '<div class="liner">' + element.time_start + '</div><div class="liner-end">' + Number(element.time_start+1) + '</div>';
+                item.classList.add("item");
+                if (data.length == i + 1) {
+                    let liner = element.process.id + '<div class="liner">' + element.time_start + '</div><div class="liner-end">' + Number(element.time_start + 1) + '</div>';
                     item.innerHTML = liner;
                 } else {
                     let liner = element.process.id + '<div class="liner">' + element.time_start + '</div>';
@@ -92,6 +129,6 @@ function myLoop(data) {
             document.getElementById('wait').innerHTML = tmp;
             document.getElementById('run').style.display = 'inline-block';
         }
-    }, 1000)
+    }, 100)
 }
 
